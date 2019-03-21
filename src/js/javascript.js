532e22967471439e40d1ce596070560131d486ae
Dupratza <hugo.duprat@laposte.net>
2019-03-21 10:58:37 +0100

    absolut limitations responsive opti

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,7 +1,19 @@
 /*PARRALLAX*/
 import Rellax from "rellax";
-console.log(Rellax);
-
+import 'waypoints/lib/noframework.waypoints.min.js';
 var rellax = new Rellax('.objet', {
     center: true
 });
+/* WAYPOINT */
+
+const sections = document.querySelectorAll('section');
+sections.forEach(section => {
+    var waypoint = new Waypoint({
+        element: section,
+        handler: function(direction){
+            section.classList.add('section--reached')
+            console.log("way")
+        },
+        offset:'30%'
+    })
+})
